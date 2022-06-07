1f8179442db331ecce664249d8918d9bf80d8c78
milann97 <m.nissanka97@gmail.com>
2022-06-07 16:10:53 +1000

    updated the test suite

--- src/test/java/com/accesshq/ApiSuiteTest.java
+++ src/test/java/com/accesshq/ApiSuiteTest.java
@@ -5,28 +5,42 @@ import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
 public class ApiSuiteTest {
-
+    String authKey = "a961b8ec-a8f3-4f67-8189-bd66f132cc29";
+    String postSearch = "https://digitalapi.auspost.com.au/postcode/search.json";
+    String postcode = "localities.locality.postcode";
     @Test
     public void AusPostStatusTest() {
         given().relaxedHTTPSValidation().
-            header("auth-key", "a961b8ec-a8f3-4f67-8189-bd66f132cc29").
+            header("auth-key", authKey).
             params("q", "North Rocks").
             params("state", "NSW").
         when().
-            get("https://digitalapi.auspost.com.au/postcode/search").
+            get(postSearch).
         then().
             assertThat().statusCode(200);
     }
 
     @Test
-    public void AusPostLocationTest() {
+    public void AusPostLocationSingleTest() {
         given().relaxedHTTPSValidation().
-            header("auth-key", "a961b8ec-a8f3-4f67-8189-bd66f132cc29").
-            params("q","Parramatta").
+            header("auth-key", authKey).
+            params("q","North Rocks").
             params("state", "NSW").
         when().
-            get("https://digitalapi.auspost.com.au/postcode/search.json").
+            get(postSearch).
         then().
-            assertThat().body("localities.locality.postcode",equalTo(2151));
+            assertThat().body(postcode,equalTo(2151));
+    }
+
+    @Test
+    public void AusPostLocationMultiTest() {
+        given().relaxedHTTPSValidation().
+            header("auth-key",authKey).
+            params("q","Parramatta").
+            params("state","NSW").
+        when().
+            get(postSearch).
+        then().
+            assertThat().body(postcode, hasItem(2151));
     }
 }
